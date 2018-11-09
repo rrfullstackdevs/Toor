3443c4dfac1164992191565b14aa5065411ce1a4
rafaelalbuquerque9 <36391894+rafaelalbuquerque9@users.noreply.github.com>
2018-11-09 18:57:16 -0400

    Add files via upload

---
+++ contagem.php
@@ -0,0 +1,11 @@
+<html>
+<title>essa porra</title>
+	<body>
+		<?php 
+		 
+   			 $URL_ATUAL= "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    		 echo $URL_ATUAL;
+		?>
+
+	</body>
+</html> 
